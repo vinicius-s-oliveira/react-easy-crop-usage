83264eb36e645a280051faecfa43d0ba26b5e479
vinicius.s.oliveira <vinicius.s.oliveira@accenture.com>
2024-09-18 16:50:29 -0300

    feat: adds image uploading from react-easy-crop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import Cropper, { Area } from "react-easy-crop";
+
+import "./App.css";
+import { getCroppedImg } from "./utils";
 
 function App() {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
-
   const [image, setImage] = useState("");
-  const [preview, setPreview] = useState("");
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+  const [croppedImage, setCroppedImage] = useState(null);
 
   const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
-    console.log(croppedArea, croppedAreaPixels);
+    setCroppedAreaPixels(croppedAreaPixels);
+  };
+
+  const readFile = (file) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => resolve(reader.result), false);
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const onFileChange = async (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const imageDataUrl = await readFile(file);
+      setImage(imageDataUrl);
+    }
   };
 
   useEffect(() => {
-    console.log("image:", image);
-  }, [image]);
+    const getCroppedImage = async () => {
+      try {
+        if (croppedAreaPixels) {
+          const croppedImage = await getCroppedImg(image, croppedAreaPixels);
+          console.log("donee", { croppedImage });
+          setCroppedImage(croppedImage);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCroppedImage();
+  }, [croppedAreaPixels, image]);
 
   return (
     <React.Fragment>
@@ -31,11 +61,47 @@ function App() {
                 que 00mb e tamanho mínimo de 500 - 500 px para fazer upload.
               </p>
             </div>
-            <div className="crop">Image Content</div>
+
+            <div className="crop">
+              {image ? (
+                <>
+                  <div className="cropImage">
+                    <Cropper
+                      image={image}
+                      crop={crop}
+                      zoom={zoom}
+                      aspect={4 / 3}
+                      onCropChange={setCrop}
+                      onCropComplete={onCropComplete}
+                      onZoomChange={setZoom}
+                    />
+                  </div>
+                  <div className="controllers">
+                    <input
+                      value={zoom}
+                      type="range"
+                      id="volume"
+                      name="volume"
+                      min={1}
+                      max={3}
+                      step={0.001}
+                      // aria-labelledby="Zoom"
+                      onChange={(e) => {
+                        setZoom(e.target.value);
+                        console.log("onChange > zoom", e.target);
+                      }}
+                    />
+                  </div>
+                </>
+              ) : (
+                <>No image selected</>
+              )}
+            </div>
+
             <div className="preview">
               Preview
-              {preview ? (
-                <img src="./src/assets/earth.jpg" alt="earth" />
+              {croppedImage ? (
+                <img src={croppedImage} alt="cropped" />
               ) : (
                 <div>No preview inserted</div>
               )}
@@ -49,7 +115,7 @@ function App() {
               <input
                 id="upload"
                 type="file"
-                onChange={() => {}}
+                onChange={onFileChange}
                 accept="image/*"
               />
             </div>
@@ -60,42 +126,6 @@ function App() {
             </div>
           </div>
         </div>
-
-        {/* <div className="imageContainer">
-        <img src="./src/assets/earth.jpg" alt="earth" />
-        <button onClick={() => setImage("./src/assets/earth.jpg")}>Crop</button>
-      </div>
-
-      <div className="imageContainer">
-        <img src="./src/assets/red-mountains.jpg" alt="earth" />
-        <button onClick={() => setImage("./src/assets/red-mountains.jpg")}>
-          Crop
-        </button>
-      </div> */}
-        {/* <div className="crop">
-        <Cropper
-          image="./src/assets/red-mountains.jpg"
-          crop={crop}
-          zoom={zoom}
-          aspect={4 / 3}
-          onCropChange={setCrop}
-          onCropComplete={onCropComplete}
-          onZoomChange={setZoom}
-        />
-      </div> */}
-        {/* {image && (
-        <div style={{ position: "absolute", backgroundColor: "blue" }}>
-          <Cropper
-            image='./src/assets/red-mountains.jpg'
-            crop={crop}
-            zoom={zoom}
-            aspect={4 / 3}
-            onCropChange={setCrop}
-            onCropComplete={onCropComplete}
-            onZoomChange={setZoom}
-          />
-        </div>
-      )} */}
       </div>
     </React.Fragment>
   );
